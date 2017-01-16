024dee848a54dc42e61c2498de3fbeb8469cdd80
Justin Krup <mazlix@gmail.com>
2017-01-15 22:01:59 -0500

    get version

--- src/dockerfile.js
+++ src/dockerfile.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 class Dockerfile {
   constructor() {
     // Set node to correct version based on meteor version (1.4+ vs 1.3-)
+    const data = fs.readFileSync('.meteor/versions', {
+      encoding: 'utf8'
+    });
     const version = data.match(/\nmeteor@(.*)\n/)[1];
     const microVersion = version.split('.')[1];
     this.dockerImage = (parseInt(microVersion, 10) < 4)
@@ -12,9 +15,6 @@ class Dockerfile {
     // Determine bundle name (it's based on meteor directory)
     const pwd = process.env.PWD;
     const builddir = pwd.split('/')[pwd.split('/').length - 1];
-    data = fs.readFileSync('.meteor/versions', {
-      encoding: 'utf8'
-    });
     this.buildzip = `${builddir}.tar.gz`;
   }
 
