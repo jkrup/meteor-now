e4fe450887c9b229be44e72f53c85691c416ba9e
Mirza Joldic <mirza.joldic@gmail.com>
2017-01-16 22:48:54 -0500

    added meteor-settings functionality

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import minimist from 'minimist';
+import logger from './logger';
 
 const isStringJson = (string) => {
   try {
@@ -10,10 +12,10 @@ const isStringJson = (string) => {
 };
 
 const readFile = async (path) => {
-  return new Promise((resolve, reject) => {
-    const data = fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+  return new Promise((resolve) => {
+    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
       if (err) {
-        reject('no meteor settings file found');
+        logger(`could not read ${path}`);
       } else {
         resolve(data);
       }
@@ -21,7 +23,35 @@ const readFile = async (path) => {
   });
 };
 
+const getNodeEnv = () => {
+  const args = minimist(process.argv.slice(2));
+  let env = 'development';
+  try {
+    env = args.e
+      .filter(arg => arg.split('=')[0] === 'NODE_ENV')[0].split('=')[1];
+  } catch (e) {
+    // NODE_ENV was not passed in (-e NODE_ENV=production)
+    // ignore error, nodeEnv will be 'development'
+  }
+  return env;
+};
+
+const didPassInMeteorSettings = () => {
+  const args = minimist(process.argv.slice(2));
+  try {
+    // TODO possibly better way of doing this
+    args.e
+      .filter(arg => arg.split('=')[0] === 'METEOR_SETTINGS')[0].split('=')[1];
+  } catch (e) {
+    // METEOR_SETTINGS argument was not passed
+    return false;
+  }
+  return true;
+};
+
 export {
   isStringJson,
   readFile,
-}
+  getNodeEnv,
+  didPassInMeteorSettings,
+};
