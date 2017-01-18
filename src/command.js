034125615bea7ae82a9d46d120797b9a6e4003c7
Mirza Joldic <mirza.joldic@gmail.com>
2017-01-18 13:55:24 -0500

    commands now log their output if true is passed as second argument

--- src/command.js
+++ src/command.js
@@ -2,17 +2,21 @@ import Spawner from 'promise-spawner';
 import logger from './logger';
 
 export default class Command {
-  constructor(command) {
+  constructor(command, inheritIo = false) {
     this.command = command;
     const modifiers = {
       out(d) { return d; },
       err: 'this is an error: ',
     };
+
     this.spawner = new Spawner(modifiers, {
-      stdio: 'inherit',
+      stdio: inheritIo ? 'inherit' : 'ignore',
     });
-    this.spawner.out.pipe(process.stdout);
-    this.spawner.err.pipe(process.stdout);
+
+    if (inheritIo) {
+      this.spawner.out.pipe(process.stdout);
+      this.spawner.err.pipe(process.stdout);
+    }
   }
   run = () => {
     logger(`running command: ${this.command}`);
