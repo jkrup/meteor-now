c8178d8ad92738ce81da36341487ce1c44b800c5
Mirza Joldic <mirza.joldic@gmail.com>
2017-09-27 21:42:02 -0400

    commenting meteor.js

--- src/api/meteor.js
+++ src/api/meteor.js
@@ -4,6 +4,7 @@ import { readFile } from './files';
 import { getEnvironmentVariable } from './args';
 import logger from './logger';
 
+// build the meteor app by using meteor build
 export const buildMeteorApp = async () => {
   logger('building app');
   await spawnProcess(
@@ -18,16 +19,20 @@ export const buildMeteorApp = async () => {
   logger('done building');
 };
 
+// get the full meteor release version
 export const getVersion = async () => {
   const release = await readFile('.meteor/release');
   return release.match(/METEOR@(.*)\r?\n/)[1];
 };
 
+// get the minor version number of the meteor release
 export const getMicroVersion = async () => {
   const version = await getVersion();
   return version.split('.')[1];
 };
 
+// get meteor settings by checking for settings.json files
+// uses NODE_ENV to determine which settings file to load
 export const getMeteorSettings = async () => {
   const nodeEnv = getEnvironmentVariable('NODE_ENV');
   if (nodeEnv) {
@@ -42,5 +47,7 @@ export const getMeteorSettings = async () => {
   return null;
 }
 
+// check to see if server only flag should be
+// passed to meteor build
 export const shouldBeServerOnly = () =>
   parseInt(getMicroVersion(), 10) < 3 ? false : true;
