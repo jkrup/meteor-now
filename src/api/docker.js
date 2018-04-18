93c0e64cb2bff4188c9749a593bca3164008a4c8
Leon <leon.machens@mobilehead.de>
2018-04-18 05:55:42 +0000

    add healthcheck js

--- src/api/docker.js
+++ src/api/docker.js
@@ -36,6 +36,42 @@ export const getDependencyInstallScripts = (deps = getDeps('deps')) => {
     );
 };
 
+const getHealthcheckPath = () => getEnvironmentVariable('HEALTHCHECK_PATH') || '/';
+
+const getHealthcheckTimeout = () => getEnvironmentVariable('HEALTHCHECK_TIMEOUT') || 5000;
+
+// the call timeout of the healthcheck has to be longer than the actual timeout
+const getHealthcheckCallTimeout = () => `${Math.parseInt(getHealthcheckTimeout() / 1000 + 5)}s`;
+
+const getHealthcheckInterval = () => getEnvironmentVariable('HEALTHCHECK_INTERVAL') || '20s';
+
+const getHealthcheckRetries = () => getEnvironmentVariable('HEALTHCHECK_RETRIES') || '3';
+
+// construct the healthceck contents
+const getHealthcheckFileContents = () => {
+  const path = getHealthcheckPath();
+  const timeout = getHealthcheckTimeout();
+  
+  return `const http = require('http');
+
+const options = {  
+  host: 'localhost',
+  port: 3000,
+  path: ${path},
+  timeout : ${timeout}
+};
+
+const request = http.request(options, res => {  
+  res.statusCode == 200 ? process.exit(0) : process.exit(1);
+});
+
+request.on('error', err => {  
+  process.exit(1);
+});
+
+request.end();`;
+};
+
 // construct the Dockerfile contents
 export const getDockerfileContents = async () => {
   // check if user pass any --deps to install in the image
@@ -63,7 +99,11 @@ WORKDIR ../../
 ${includeMongo ? 'COPY supervisord.conf /etc/supervisor/supervisord.conf' : ''}
 EXPOSE 3000
 ${includeMongo ? 'CMD ["supervisord"]' : 'CMD ["node", "main.js"]'}
-HEALTHCHECK --interval=20s --timeout=8s --retries=3 CMD curl -f http://localhost:3000/ || exit 1`;
+HEALTHCHECK \
+  --interval=${getHealthcheckInterval} \
+  --timeout=${getEnvironmentVariable('HEALTHCHECK_TIMEOUT') || '10s'} \
+  --retries=${getHealthcheckRetries} \
+  CMD node healthcheck.js`;
 };
 
 // construct the supervisord contents
@@ -92,6 +132,7 @@ export const prepareDockerConfig = async () => {
       // create a supervisord.conf file to run mongodb inside the container
       await writeFile(`${meteorNowBuildPath}/supervisord.conf`, getSupervisordFileContents());
     }
+    await writeFile(`${meteorNowBuildPath}/healthcheck.js`, getHealthcheckFileContents());
     logger.succeed();
   } catch (e) {
     // eslint-disable-next-line
