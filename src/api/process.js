b2541685915a57e083a02e81efd74e6ac666d9fc
Josh <josh@joshjoe.com>
2018-08-15 16:34:58 -0500

    In order for alias to work we need to resolve the promise after exit

--- src/api/process.js
+++ src/api/process.js
@@ -3,24 +3,28 @@ import { isDebug } from './args';
 import logger from './logger';
 
 export default (cmd, args) => {
-  logger.debug(`$ ${cmd}`, ...args);
+  if (isDebug()) {
+    logger.debug(`$ ${cmd}`, ...args);
+  }
+
   return new Promise((resolve, reject) => {
     const child = spawn(cmd, args, {
       stdio: isDebug() ? 'inherit' : [process.stdin, 'pipe', process.stderr],
       shell: true,
     });
+    let processData = '';
 
     child.on('exit', (code, signal) => {
       if (code !== 0) {
         reject({ code, signal });
       } else {
-        resolve();
+        resolve(processData);
       }
     });
 
     if (child.stdout) {
       child.stdout.on('data', (data) => {
-        resolve(data.toString());
+        processData = data.toString();
       });
     }
   });
