32a6439b0b7e417103e2491142dc4641fa457e07
Josh <josh@joshjoe.com>
2018-08-15 11:07:06 -0500

    testing for --alias

--- src/api/__tests__/now.js
+++ src/api/__tests__/now.js
@@ -3,7 +3,7 @@ import { constructNowOptions } from '../now';
 
 describe('now test', () => {
   test('it should construct the correct now options', async () => {
-    process.argv = ['node', '-e', 'MONGO_URL=mongodb://127.0.0.1:27017', '--deps', 'imagemagick', '-e', 'METEOR_SETTINGS=\'{ "foo": "bar" }\''];
+    process.argv = ['node', '-e', 'MONGO_URL=mongodb://127.0.0.1:27017', '--deps', 'imagemagick', '-e', 'METEOR_SETTINGS=\'{ "foo": "bar" }\'', '--alias', 'abc.com'];
     const nowOptions = await constructNowOptions();
     expect(nowOptions).toEqual([
       `${os.homedir()}/.meteor-now/build`,
