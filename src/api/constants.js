5a6486df99f03c87c62e7b8e5674d36f2c3aebd9
brianmcdonough <lumeo@me.com>
2018-08-22 18:35:37 -0700

    add PROJECT_NAME to environmental variables as an option

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,8 @@
 import os from 'os';
+import {
+  getEnvironmentVariable,
+  getEnvironmentVariables
+} from './args';
 
 const isWin = /^win/.test(process.platform);
 
@@ -8,11 +12,16 @@ export const getFolderName = (path, isWinOverride = isWin) => {
   return pathParts[pathParts.length - 1];
 };
 
+export const getProjectName = async () => {
+  const environmentVariables = await getEnvironmentVariables();
+  return getEnvironmentVariable('PROJECT_NAME', environmentVariables)
+};
+
 // run immediately
-export const projectName = (() => getFolderName(process.cwd()))();
+export const projectName = getProjectName() || (() => getFolderName(process.cwd()))();
 export const homePath = os.homedir();
 export const meteorNowBuildPath = isWin ? `${homePath}\\.meteor-now\\build` : `${homePath}/.meteor-now/build`;
 export const tarFileName = `${projectName}.tar.gz`;
-export const ignoreVarsArray = ['MONGO_URL', 'ROOT_URL', 'METEOR_SETTINGS', 'PORT'];
+export const ignoreVarsArray = ['MONGO_URL', 'ROOT_URL', 'METEOR_SETTINGS', 'PORT', 'PROJECT_NAME'];
 export const ignoreOptionsArray = ['deps', '_', '$0', 'help', 'version', 'nosplit'];
 export const logPrefix = '[METEOR-NOW] - ';
