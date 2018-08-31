57291d2e8d8c8043de0164433bf06f2b5e0b99fe
nzouabi <zouabi.nabil@live.com>
2018-08-31 15:47:04 +0200

    Separate apt-get update from includeMongo because we need if we install only deps

--- src/api/docker.js
+++ src/api/docker.js
@@ -43,15 +43,15 @@ export const getDockerfileContents = async () => {
   // check to see if mogno should be included
   const includeMongo = shouldIncludeMongo();
   return `FROM ${dockerImage}
+${includeMongo ? `RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+RUN echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list` :''}
+${includeMongo || deps ? 'RUN apt-get update' : ''}
 ${includeMongo
-    ? `RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-RUN echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-RUN apt-get update
-${deps ? getDependencyInstallScripts(deps) : ''}
-RUN apt-get install -y mongodb-org
+    ? `RUN apt-get install -y mongodb-org
 RUN apt-get install -y supervisor
 VOLUME ["/data/db"]`
     : ''}
+${deps ? getDependencyInstallScripts(deps) : ''}    
 LABEL name="${projectName}"
 COPY bundle/programs/server/package.json /usr/src/app/bundle/programs/server/package.json
 WORKDIR /usr/src/app/bundle/programs/server
